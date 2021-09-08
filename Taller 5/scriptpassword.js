688ea22398e7ee93cb74bcce01f83d226f77a38a
Freed219 <jose2190044@correo.uis.edu.co>
2021-09-08 10:42:30 -0500

    2C

--- Taller 5/scriptpassword.js
+++ Taller 5/scriptpassword.js
@@ -1,20 +1,56 @@
-const password = document.getElementById('aligned-password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+var myInput = document.getElementById("aligned-password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-form.addEventListener('submit', (e)=>{
-    let messages = []
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
 
-    if(password.value.length <= 15) {
-        messages.push('La contrasena debe contener minimo 15 caracteres')
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+  }
+  
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {
+      capital.classList.remove("invalid");
+      capital.classList.add("valid");
+    } else {
+      capital.classList.remove("valid");
+      capital.classList.add("invalid");
     }
-
-    if(password.value.length >= 20) {
-        messages.push('La contrasena no puede contener mas de 20 caracteres')
+  
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    } else {
+      number.classList.remove("valid");
+      number.classList.add("invalid");
     }
-
-    if (messages.length >0) {
-    e.preventDefault()  
-    errorElement.innerText = messages.join(', ')
+  
+    // Validate length
+    if(myInput.value.length >= 15) {
+      length.classList.remove("invalid");
+      length.classList.add("valid");
+    } else {
+      length.classList.remove("valid");
+      length.classList.add("invalid");
     }
-})
+  }
