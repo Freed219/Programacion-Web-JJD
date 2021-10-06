4c159c3676d269e1338afe2ffc6cc81e0295fc81
Freed219 <jose2190044@correo.uis.edu.co>
2021-10-06 12:16:55 -0500

    5C

--- Taller 7/script.js
+++ Taller 7/script.js
@@ -1,51 +1,53 @@
-$(document).ready(iniciarEventos);
-function iniciarEventos(){
-    llenarDepartamentos();
-    $("#departamento").change(llenarMunicipios);
+$(document).ready(ini_evento);
+function ini_evento() {
+    fill_departamento();
+    $("#departamento").change(fill_municipio);
 }
-function llenarDepartamentos(){
+
+function fill_departamento() {
     fetch('https://www.datos.gov.co/resource/xdk5-pm3f.json')
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        let regiones = json;
-        let departamentos=[];
-        let html='<option value="">Seleccione...</option>';
-        regiones.forEach(element => {
-            /* html+='<option>'+element.departamento+'</option>'; */
-            departamentos.push(element.departamento)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            let jsonregion = json;
+            let departamentos = [];
+            let html = '<option value="">Seleccione...</option>';
+            jsonregion.forEach(element => {
+                departamentos.push(element.departamento)
+            });
+            sinRepetidos = departamentos.unique();
+            sinRepetidos.sort();
+            sinRepetidos.forEach(element => {
+                html += '<option>' + element + '</option>';
+            });
+            document.getElementById('departamento').innerHTML = html;
         });
-        sinRepetidos=departamentos.unique();
-        sinRepetidos.sort();/* Se ordena el vector alfabeticamente */
-        sinRepetidos.forEach(element =>{
-            html+='<option>'+element+'</option>';
-        });
-        document.getElementById('departamento').innerHTML=html;
-    });
 }
-function llenarMunicipios(){
+
+function fill_municipio() {
     let select = document.getElementById('departamento');
     let departamento = select.options[select.selectedIndex].value;
     fetch('https://www.datos.gov.co/resource/xdk5-pm3f.json')
-    .then(response => response.json())
-    .then(json => {
-        let regiones = json;
-        let municipios=[];
-        let html='';
-        regiones.forEach(element => {
-            /* departamentos.push(element.departamento) */
-            if(element.departamento==departamento){
-                municipios.push(element.municipio);
-            }
+        .then(response => response.json())
+        .then(json => {
+            let jsonregion = json;
+            let municipios = [];
+            let html = '';
+            jsonregion.forEach(element => {
+                if (element.departamento == departamento) {
+                    municipios.push(element.municipio);
+                }
+            });
+            municipios.sort();
+            municipios.forEach(element => {
+                html += '<option>' + element + '</option>';
+            });
+            document.getElementById('municipio').innerHTML = html;
         });
-        municipios.sort();
-        municipios.forEach(element =>{
-            html+='<option>'+element+'</option>';
-        });
-        document.getElementById('municipio').innerHTML=html;
-    });
 }
-/* Función para eliminar elementos repetidos de un array */
-Array.prototype.unique=function(a){
-    return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+
+Array.prototype.unique = function (a) {
+    return function () { return this.filter(a) }
+}(function (a, b, c) {
+    return c.indexOf(a, b + 1) < 0
 });
