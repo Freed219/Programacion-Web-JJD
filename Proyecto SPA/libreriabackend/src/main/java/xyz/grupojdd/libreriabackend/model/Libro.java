5141c464d0234e8017bae7372c760ba1dc90200c
Freed219 <jose2190044@correo.uis.edu.co>
2021-10-19 00:41:24 -0500

    4C

--- Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend/model/Libro.java
+++ Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend/model/Libro.java
@@ -1,12 +1,17 @@
 package xyz.grupojdd.libreriabackend.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "libro")
 public class Libro {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String nombre;
     private String paginas;
