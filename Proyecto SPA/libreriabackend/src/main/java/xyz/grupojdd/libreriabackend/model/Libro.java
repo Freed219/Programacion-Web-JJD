d3d1bf380748baf9cee7792e48221c6f984c34c9
Freed219 <jose2190044@correo.uis.edu.co>
2021-10-19 01:17:47 -0500

    4C

--- Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend/model/Libro.java
+++ Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend/model/Libro.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "libro")
 public class Libro {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -18,7 +20,11 @@ public class Libro {
     private String year_creacion;
     private boolean vivo;
 
+    @ManyToOne
+    @JoinColumn (name = "id_autor")
     private Autor autor;
+    @ManyToOne
+    @JoinColumn (name = "id_editorial")
     private Editorial editorial;
     
 }
