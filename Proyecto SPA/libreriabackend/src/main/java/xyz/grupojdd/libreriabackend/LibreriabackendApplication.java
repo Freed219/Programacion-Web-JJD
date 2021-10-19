bd7cc87a2a1a1ab37e3a13249747b3a934331d85
Freed219 <jose2190044@correo.uis.edu.co>
2021-10-18 21:44:09 -0500

    Crear

---
+++ Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend/LibreriabackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.grupojdd.libreriabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriabackendApplication.class, args);
+	}
+
+}
---
+++ Proyecto SPA/libreriabackend/src/test/java/xyz/grupojdd/libreriabackend/LibreriabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.grupojdd.libreriabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
