61abe40d7c4bc46fb207a2c2dc61c9df960090c0
ImJuandix <83036872+ImJuandix@users.noreply.github.com>
2021-10-19 13:38:32 -0500

    Porfi hpta

--- Proyecto SPA/libreriafront/src/app/app.component.ts
+++ Proyecto SPA/libreriafront/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AutorService } from './services/Autor/autor.service';
+import { EditorialService } from './services/Editorial/editorial.service';
+import { LibroService } from './services/Libro/libro.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'libreriafront';
+export class AppComponent implements OnInit{
+  
+  autorForm: FormGroup;
+
+
+constructor(
+  public fb: FormBuilder,
+  public autorService: AutorService,
+  public editorialService: EditorialService,
+  public libroService: LibroService
+){
+
+}
+ngOnInit(): void {
+  this.autorForm = this.fb.group({
+  nombre : ['', Validators.required],
+  id : ['',Validators.required],
+ 
+
+})
+}
+guardar(): void{
+  
+}
 }
