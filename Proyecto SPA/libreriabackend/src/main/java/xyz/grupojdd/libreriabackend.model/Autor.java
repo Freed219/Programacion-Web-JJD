bdecf0c5f21f1f2f43dfb2dbe5899372e4c8fc04
Freed219 <jose2190044@correo.uis.edu.co>
2021-10-18 23:23:30 -0500

    2C

---
+++ Proyecto SPA/libreriabackend/src/main/java/xyz/grupojdd/libreriabackend.model/Autor.java
@@ -0,0 +1,10 @@
+package xyz.grupojdd.libreriabackend.model;
+
+public class Autor {
+    private String nombre;
+    private String fecha_nacimiento;
+    private boolean Alive;
+
+    private Editorial editorial;
+    private Libro libro;
+}
